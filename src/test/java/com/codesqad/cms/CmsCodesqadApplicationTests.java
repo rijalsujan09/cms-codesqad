503e547c40445fa1615801df8acede5d7743a838
rijalsujan09 <rijalsujan09@gmail.com>
2024-09-02 23:36:13 -0400

    added some test properties

--- src/test/java/com/codesqad/cms/CmsCodesqadApplicationTests.java
+++ src/test/java/com/codesqad/cms/CmsCodesqadApplicationTests.java
@@ -2,8 +2,10 @@ package com.codesqad.cms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CmsCodesqadApplicationTests {
 
 	@Test
